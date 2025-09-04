bcd38f5ce50afe14fd70586a46f130349ada73e7
sherlock <wisdomnji@gmail.com>
2025-09-04 12:23:05 +0100

    update: pausing others, using jitpack

--- smswithoutborders_libsmsmms/src/main/java/com/afkanerd/smswithoutborders_libsmsmms/extensions/context/Database.kt
+++
@@ -1,8 +0,0 @@
-package com.afkanerd.smswithoutborders_libsmsmms.extensions.context
-
-import android.content.Context
-import com.afkanerd.smswithoutborders_libsmsmms.data.DatabaseImpl
-
-fun Context.getDatabase(): DatabaseImpl {
-    return DatabaseImpl.getDatabaseImpl(this)
-}
