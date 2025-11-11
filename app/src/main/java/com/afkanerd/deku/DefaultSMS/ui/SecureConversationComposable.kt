b1857932c66fe9902737e75c2fe6a7d48bf5e8e4
sherlock <wisdomnji@gmail.com>
2025-11-11 18:09:57 +0100

    update: inform when encryption is on

--- app/src/main/java/com/afkanerd/deku/DefaultSMS/ui/SecureConversationComposable.kt
+++ app/src/main/java/com/afkanerd/deku/DefaultSMS/ui/SecureConversationComposable.kt
@@ -1,36 +1,23 @@
 package com.afkanerd.deku.DefaultSMS.ui
 
-import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewModelScope
-import com.afkanerd.deku.DefaultSMS.BuildConfig
-import com.afkanerd.deku.DefaultSMS.R
-import com.afkanerd.deku.DefaultSMS.ui.viewModels.SecureConversationViewModel
 import com.afkanerd.deku.DefaultSMS.ui.components.SecureRequestAcceptModal
+import com.afkanerd.deku.DefaultSMS.ui.viewModels.SecureConversationViewModel
 import com.afkanerd.smswithoutborders.libsignal_doubleratchet.EncryptionController
 import com.afkanerd.smswithoutborders.libsignal_doubleratchet.SavedEncryptedModes
 import com.afkanerd.smswithoutborders.libsignal_doubleratchet.getEncryptionModeStates
-import com.afkanerd.smswithoutborders_libsmsmms.extensions.context.getSubscriptionName
-import com.afkanerd.smswithoutborders_libsmsmms.ui.components.SimChooser
 import com.google.gson.Gson
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Composable
 fun SecureConversationComposable(viewModel: SecureConversationViewModel ) {
     val context = LocalContext.current
 
     val mode = viewModel.mode
-    val showModal = viewModel.trigger
+    val showModal = viewModel.anythingTrigger
 
     viewModel.address?.let { address ->
         val state by context.getEncryptionModeStates(address).collectAsState("")
@@ -48,12 +35,14 @@ fun SecureConversationComposable(viewModel: SecureConversationViewModel ) {
                     == EncryptionController.SecureRequestMode.REQUEST_ACCEPTED) {
                     viewModel.mode = EncryptionController.SecureRequestMode.REQUEST_ACCEPTED
 
-                    if(BuildConfig.DEBUG)
-                        Toast.makeText(context,
-                            context.getString(R.string.secure_conversation),
-                            Toast.LENGTH_LONG).show()
+//                    if(BuildConfig.DEBUG)
+//                        Toast.makeText(context,
+//                            context.getString(R.string.secure_conversation),
+//                            Toast.LENGTH_LONG).show()
+                    viewModel.setIsSecured(true)
                 }
             }
+            else viewModel.setIsSecured(false)
         }
 
         SecureRequestAcceptModal(
